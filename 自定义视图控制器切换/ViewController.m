63d84c9a2799ab73099e3733cb7144ab0a135741
花生 <bjlidachuan@163.com>
2016-06-15 14:56:55 +0800

    Initial Commit

---
+++ 自定义视图控制器切换/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  自定义视图控制器切换
+//
+//  Created by 花生 on 16/6/15.
+//  Copyright © 2016年 花生. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 自定义视图控制器切换/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  自定义视图控制器切换
+//
+//  Created by 花生 on 16/6/15.
+//  Copyright © 2016年 花生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
